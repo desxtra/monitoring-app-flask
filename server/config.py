9f701e6a9fc8114fc76dc7d372098d19fb754352
desxtra <malvinsans@gmail.com>
2025-09-16 10:24:00 +0700

    FIX dashboard and FIX group1

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     # Server Configuration
     HOST = os.getenv('FLASK_HOST', '0.0.0.0')
-    PORT = int(os.getenv('FLASK_PORT', 5000))
+    PORT = int(os.getenv('FLASK_PORT', 6000))
     DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
     
     # Data retention (in seconds)
